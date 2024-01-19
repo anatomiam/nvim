e11f5e0ef5ba84da7402d022a6738bb97e0928a0
Eric Dulcet <eric.dulcet@wirecutter.com>
2024-01-19 15:30:58 -0500

    Transparent background, autoformatting

--- lua/plugins/theme-gruvbox.lua
+++ lua/plugins/theme-gruvbox.lua
@@ -1,8 +1,12 @@
 return {
     -- add gruvbox
-    { "ellisonleao/gruvbox.nvim", opts = {
-        transparent_mode = true,
-    } },
+    {
+        "ellisonleao/gruvbox.nvim",
+        opts = {
+            transparent_mode = true,
+            background_colour = "#000000",
+        },
+    },
     -- Configure LazyVim to load gruvbox
     {
         "LazyVim/LazyVim",
